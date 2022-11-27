6bc7be52ac206b0a63159cd19241c07fb262f1f1
signinme <owei_@outlook.com>
2022-11-27 15:10:59 +0800

    add problem p140

---
+++ template/template.cpp
@@ -0,0 +1,37 @@
+/**
+ * @file    template.cpp
+ * @name    p___模板程序
+ * @date    2022-11-20
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+using namespace std;
+
+
+/********************************/
+/*     以下是你需要提交的代码     */
+/********************************/
+class Solution {
+public:
+    int solve(std::vector<vector<std::string>> &old_chart, std::vector<std::vector<std::string>> &new_chart) {
+        // 这里填写你的代码
+    }
+};
+/********************************/
+/*     以上是你需要提交的代码     */
+/********************************/
+
+int main() {
+    
+}
